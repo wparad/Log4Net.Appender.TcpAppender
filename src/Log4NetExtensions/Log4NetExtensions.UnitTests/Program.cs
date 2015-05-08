c9734904b7f6a5a23be3eece385d108ee9600f18
Warren Parad <wparad@cimpress.com>
2015-05-08 11:20:14 -0400

    Create TcpAppender class.

---
+++ src/Log4NetExtensions/Log4NetExtensions.UnitTests/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+using log4net;
+using log4net.Appender;
+
+namespace Log4NetExtensions.UnitTests
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            SetupTcp();
+
+            var serializer = new JavaScriptSerializer();
+            Thread.Sleep(2);
+            var tasks = new int[20].Select((_, index) => Task.Factory.StartNew(() =>
+            {
+                var logger = LogManager.GetLogger(string.Format("Logger: {0}", index));
+                for (var loop = 0; loop < 10000; loop++)
+                {
+                    logger.Info(serializer.Serialize(new
+                    {
+                      Type = "Log4NetExtensions.UnitTests",
+                      Loop = loop,
+                      Index = index
+                    }));
+                    Thread.Sleep(1);
+                }
+            }));
+            Task.WaitAll(tasks.ToArray());
+        }
+
+        public static void SetupUdp()
+        {
+            var layout = new log4net.Layout.PatternLayout();
+            //var appender = new TcpAppender(5140, IPAddress.Loopback) { Layout = layout};
+            var appender = new UdpAppender{ Layout = layout, RemoteAddress = IPAddress.Loopback, RemotePort = 5142};
+            layout.ActivateOptions();
+            appender.ActivateOptions();
+
+            var layout2 = new log4net.Layout.PatternLayout();
+            //var appender = new TcpAppender(5140, IPAddress.Loopback) { Layout = layout};
+            var appender2 = new UdpAppender{ Layout = layout2, RemoteAddress = IPAddress.Loopback, RemotePort = 5142};
+            layout2.ActivateOptions();
+            appender2.ActivateOptions();
+
+            var layout3 = new log4net.Layout.PatternLayout();
+            //var appender = new TcpAppender(5140, IPAddress.Loopback) { Layout = layout};
+            var appender3 = new UdpAppender{ Layout = layout3, RemoteAddress = IPAddress.Loopback, RemotePort = 5142};
+            layout3.ActivateOptions();
+            appender3.ActivateOptions();
+
+            log4net.Config.BasicConfigurator.Configure(appender, appender2, appender3);
+        }
+
+        public static void SetupTcp()
+        {
+            var layout = new log4net.Layout.PatternLayout();
+            var appender = new TcpAppender(5140, IPAddress.Loopback) { Layout = layout};
+            layout.ActivateOptions();
+            appender.ActivateOptions();
+
+            var layout2 = new log4net.Layout.PatternLayout();
+            var appender2 = new TcpAppender(5140, IPAddress.Loopback) { Layout = layout2};
+            layout2.ActivateOptions();
+            appender2.ActivateOptions();
+
+                var layout3 = new log4net.Layout.PatternLayout();
+            var appender3 = new TcpAppender(5140, IPAddress.Loopback) { Layout = layout3};
+            layout3.ActivateOptions();
+            appender3.ActivateOptions();
+
+            log4net.Config.BasicConfigurator.Configure(appender, appender2, appender3);
+        }
+    }
+}
---
+++ src/Log4NetExtensions/TcpAppender.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Globalization;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using log4net.Appender;
+using log4net.Core;
+using log4net.Util;
+
+namespace Log4NetExtensions 
+{
+	public class TcpAppender : AppenderSkeleton
+	{
+	    public IPAddress RemoteAddress { get; set; }
+	    public int RemotePort { get; set; }
+	    public Encoding Encoding { get; set; }
+	    protected TcpClient Client { get; set; }
+
+		public override void ActivateOptions()
+		{
+			base.ActivateOptions();
+
+            try
+            {
+                Client = new TcpClient();
+                Client.Connect(RemoteAddress, RemotePort);
+            }
+			catch (Exception ex) 
+			{
+				ErrorHandler.Error("Could not initialize the TcpClient.", ex, ErrorCode.GenericFailure);
+				Client = null;
+			}
+		}
+
+		protected override void Append(LoggingEvent loggingEvent) 
+		{
+			try 
+		    {
+                //The remote can close the connection, in that case it should be reopened.
+		        if (!Client.Connected) { Client.Connect(RemoteAddress, RemotePort);}
+		        var buffer = Encoding.GetBytes(RenderLoggingEvent(loggingEvent).ToCharArray());
+			    Client.GetStream().Write(buffer, 0, buffer.Length);
+			} 
+			catch (Exception ex) 
+			{
+				ErrorHandler.Error(string.Format("Unable to send logging event to remote host {0} on port {1}.",
+                    RemoteAddress, RemotePort), ex,  ErrorCode.WriteFailure);
+			}
+		}
+
+		override protected bool RequiresLayout
+		{
+			get { return true; }
+		}
+
+	    public TcpAppender(int remotePort, IPAddress remoteAddress)
+	    {
+	        if (remoteAddress == null) { throw new ArgumentNullException("remoteAddress", "Remote address of the location must be specified."); }
+            RemoteAddress = remoteAddress;
+
+            if (remotePort < IPEndPoint.MinPort || remotePort > IPEndPoint.MaxPort) 
+			{
+				throw SystemInfo.CreateArgumentOutOfRangeException("value", remotePort, 
+                    string.Format("The value specified is less than {0} or greater than {1}.",
+                    IPEndPoint.MinPort.ToString(NumberFormatInfo.InvariantInfo), IPEndPoint.MaxPort.ToString(NumberFormatInfo.InvariantInfo)));
+			}
+            RemotePort = remotePort;
+
+            Encoding = Encoding.Default;
+	    }
+
+        protected override void OnClose()
+        {
+            base.OnClose();
+            if (Client != null)
+            {
+                Client.Close();
+                Client = null;
+            }
+        }
+	}
+}
