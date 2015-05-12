22c0c33ce11875557cb734986f466b5ae6b70751
Warren Parad <wparad@cimpress.com>
2015-05-11 20:18:39 -0400

    Allow layout to work with XmlConfiguration.

--- src/Log4NetExtensions/TcpAppender.cs
+++ src/Log4NetExtensions/TcpAppender.cs
@@ -7,28 +7,38 @@ using log4net.Appender;
 using log4net.Core;
 using log4net.Util;
 
-namespace Log4NetExtensions 
+namespace Log4NetExtensions
 {
 	public class TcpAppender : AppenderSkeleton
 	{
-	    public IPAddress RemoteAddress { get; set; }
+        public IPAddress RemoteAddress { get; set; }
 	    public int RemotePort { get; set; }
 	    public Encoding Encoding { get; set; }
 	    protected TcpClient Client { get; set; }
+        override protected bool RequiresLayout { get { return true; } }
+
+        public TcpAppender()
+	    {
+            Encoding = Encoding.Default;
+            RemoteAddress = IPAddress.Loopback;
+	    }
 
 		public override void ActivateOptions()
 		{
 			base.ActivateOptions();
 
-            try
-            {
-                Client = new TcpClient();
-                Client.Connect(RemoteAddress, RemotePort);
-            }
-			catch (Exception ex) 
+            if (RemoteAddress == null) { throw new Exception("Remote address of the location must be specified."); }
+
+            if (RemotePort < IPEndPoint.MinPort || RemotePort > IPEndPoint.MaxPort) 
 			{
-				ErrorHandler.Error("Could not initialize the TcpClient.", ex, ErrorCode.GenericFailure);
+				throw SystemInfo.CreateArgumentOutOfRangeException("value", RemotePort, 
+                    string.Format("The value specified is less than {0} or greater than {1}.",
+                    IPEndPoint.MinPort.ToString(NumberFormatInfo.InvariantInfo), IPEndPoint.MaxPort.ToString(NumberFormatInfo.InvariantInfo)));
 			}
+
+            Client = new TcpClient();
+            try { Client.Connect(RemoteAddress, RemotePort); }
+			catch (Exception ex) { ErrorHandler.Error("Could not initialize the TcpClient.", ex, ErrorCode.GenericFailure); }
 		}
 
 		protected override void Append(LoggingEvent loggingEvent) 
@@ -52,27 +62,6 @@ namespace Log4NetExtensions
                     RemoteAddress, RemotePort), ex,  ErrorCode.WriteFailure);
 			}
 		}
-
-		override protected bool RequiresLayout
-		{
-			get { return true; }
-		}
-
-	    public TcpAppender(int remotePort, IPAddress remoteAddress)
-	    {
-	        if (remoteAddress == null) { throw new ArgumentNullException("remoteAddress", "Remote address of the location must be specified."); }
-            RemoteAddress = remoteAddress;
-
-            if (remotePort < IPEndPoint.MinPort || remotePort > IPEndPoint.MaxPort) 
-			{
-				throw SystemInfo.CreateArgumentOutOfRangeException("value", remotePort, 
-                    string.Format("The value specified is less than {0} or greater than {1}.",
-                    IPEndPoint.MinPort.ToString(NumberFormatInfo.InvariantInfo), IPEndPoint.MaxPort.ToString(NumberFormatInfo.InvariantInfo)));
-			}
-            RemotePort = remotePort;
-
-            Encoding = Encoding.Default;
-	    }
 
         protected override void OnClose()
         {
