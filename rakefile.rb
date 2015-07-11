f537360a0289f798f46c7f0db638c02d05fc8b26
Warren Parad <wparad@gmail.com>
2015-07-10 22:17:26 -0400

    Create nuget package.

--- rakefile.rb
+++ rakefile.rb
@@ -1,13 +1,23 @@
 #!/bin/
 require 'rake'
 require 'rake/clean'
+require 'fileutils'
+require 'tmpdir'
 
+PWD = __dir__
+OUTPUT_DIR = File.join(PWD, 'output')
 SOLUTION_SLN = 'Log4NetExtensions.sln'
-task :build do
+
+directory OUTPUT_DIR
+task :build => [OUTPUT_DIR] do
   begin
     raise 'Nuget restore failed' if !system("nuget restore #{SOLUTION_SLN}")
     system('nuget install NUnit.Runners -Version 2.6.4 -OutputDirectory testrunner')
     raise 'Build failed' if !system("xbuild /p:Configuration=Release #{SOLUTION_SLN}")
+    Dir.mktmpdir do |tmp|
+      FileUtils.cp_r(Dir['src/Log4NetExtensions/bin/Release/**'], tmp)
+      raise 'Nuget packing failed' if !system("nuget pack 'TcpAppender.nuspec' -Basepath #{tmp} -OutputDirectory #{OUTPUT_DIR}")
+    end
   rescue Exception => exception
     system('ls -alR')
     raise
