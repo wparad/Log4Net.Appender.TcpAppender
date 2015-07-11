767054fa4db82c7a900d0549cc56be1c47435ce2
Warren Parad <wparad@gmail.com>
2015-07-10 22:41:07 -0400

    Create git tag and publish nuget package to github.

--- rakefile.rb
+++ rakefile.rb
@@ -4,10 +4,11 @@ require 'rake/clean'
 require 'fileutils'
 require 'tmpdir'
 
-PWD = __dir__
+PWD = File.dirname(__FILE__)
 OUTPUT_DIR = File.join(PWD, 'output')
 SOLUTION_SLN = 'Log4NetExtensions.sln'
 
+#Environment variables: http://docs.travis-ci.com/user/environment-variables/
 directory OUTPUT_DIR
 task :build => [OUTPUT_DIR] do
   begin
@@ -18,6 +19,13 @@ task :build => [OUTPUT_DIR] do
       FileUtils.cp_r(Dir['src/Log4NetExtensions/bin/Release/**'], tmp)
       raise 'Nuget packing failed' if !system("nuget pack 'TcpAppender.nuspec' -Basepath #{tmp} -OutputDirectory #{OUTPUT_DIR}")
     end
+
+    #Setup up deploy
+    puts %x[git config --global user.email "builds@travis-ci.com"]
+    puts %x[git config --global user.name "Travis CI"]
+    tag = "0.1.#{ENV['TRAVIS_BUILD_NUMBER']}"
+    puts %x[git tag #{tag} -a -m "Generated tag from TravisCI for build #{ENV['TRAVIS_BUILD_NUMBER']}"]
+    puts %x[git push --quiet https://#{ENV['GIT_TAG_PUSHER']}@github.com/wparad/Log4Net.Appender.TcpAppender.git #{tag} > /dev/null 2>&1]
   rescue Exception => exception
     system('ls -alR')
     raise
